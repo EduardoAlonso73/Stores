6416a30483c0c6efb766edf0851f2dce7fd5d170
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-14 11:25:38 -0500

    Realizamos las configuraciones para mostra el recycle view

--- app/src/main/java/com/example/appstoreunderstan/MainActivity.kt
+++ app/src/main/java/com/example/appstoreunderstan/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.appstoreunderstan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.appstoreunderstan.databinding.ActivityMainBinding
+import com.example.stores.OnClickListener
+import com.example.stores.Store
+import com.example.stores.StoreAdapter
 
-class MainActivity : AppCompatActivity() {
+
+private lateinit var  mBinding: ActivityMainBinding
+private lateinit var  mAdapter:StoreAdapter
+private lateinit var mGridLayout: GridLayoutManager
+
+class MainActivity : AppCompatActivity(),OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding= ActivityMainBinding.inflate(layoutInflater )
+        setContentView(mBinding.root)
+
+
+        mBinding.btnSave.setOnClickListener {
+            val store= Store(name=mBinding.etName.text.toString().trim())
+            mAdapter.addStore(store)
+        }
+
+        initRecycleView()
+    }
+
+
+    private  fun initRecycleView(){
+        mAdapter= StoreAdapter(mutableListOf(),this)
+        mGridLayout=GridLayoutManager(this,2)
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager=mGridLayout
+            adapter=mAdapter
+        }
     }
 }

74187a29b8bac2905fc4fa88b3dadb308e6459d3
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-14 21:55:07 -0500

    Configuracion para poder acceder a la base de datos implentando el patron singleton

--- app/src/main/java/com/example/appstoreunderstan/StoreAppplication.kt
+++ app/src/main/java/com/example/appstoreunderstan/StoreAppplication.kt
@@ -1,4 +1,14 @@
 package com.example.appstoreunderstan
 
-class StoreAppplication {
+import android.app.Application
+import androidx.room.Room
+
+class StoreAppplication: Application() {
+    companion object{
+        lateinit var  database: StoreDatabase
+    }
+    override fun onCreate() {
+        super.onCreate()
+        database= Room.databaseBuilder(this,StoreDatabase::class.java,"StoreDatabase").build()
+    }
 }

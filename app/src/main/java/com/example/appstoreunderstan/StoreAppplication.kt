77b985a9089e1d82f54479f3bbcd18c9e58ed861
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-14 20:05:19 -0500

    Definimos y configuramos la base de datos, indicando las tablas que tiene y la version

---
+++ app/src/main/java/com/example/appstoreunderstan/StoreAppplication.kt
@@ -0,0 +1,4 @@
+package com.example.appstoreunderstan
+
+class StoreAppplication {
+}
---
+++ app/src/main/java/com/example/appstoreunderstan/StoreDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.appstoreunderstan
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.stores.StoreEntity
+
+@Database(entities= arrayOf(StoreEntity::class), version = 1 )
+ abstract  class StoreDatabase: RoomDatabase() {
+
+     abstract  fun storeDoa():StoreDao
+}

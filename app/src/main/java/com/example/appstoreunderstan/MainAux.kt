74210191c1fbe2ee7956e9b097b39112242b0a55
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-19 10:04:14 -0500

    Metodos auxiliares para comunicar el Main y el Fragment

--- app/src/main/java/com/example/appstoreunderstan/MainAux.kt
+++ app/src/main/java/com/example/appstoreunderstan/MainAux.kt
@@ -1,4 +1,10 @@
 package com.example.appstoreunderstan
 
+import com.example.stores.StoreEntity
+
 interface MainAux {
+    fun hideBtnFb(isVisible:Boolean=false)
+    fun addStore(storeEntity: StoreEntity)
+    fun updateStore(storeEntity: StoreEntity)
+
 }

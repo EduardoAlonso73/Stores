673fd76b9532217e98b8c5fadb6616afd91eee3c
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-20 15:22:07 -0500

    Cambiamos la version de 1 a 2

--- app/src/main/java/com/example/appstoreunderstan/StoreDatabase.kt
+++ app/src/main/java/com/example/appstoreunderstan/StoreDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.stores.StoreEntity
 
-@Database(entities= arrayOf(StoreEntity::class), version = 1 )
+@Database(entities= arrayOf(StoreEntity::class), version = 2 )
  abstract  class StoreDatabase: RoomDatabase() {
 
      abstract  fun storeDoa():StoreDao

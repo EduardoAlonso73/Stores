22502a89c894cd2f07f5ff3e01d7b3de8f6a1f84
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-20 15:19:05 -0500

    Agregamos en la entidad StoreEntity el atributo o variable photoUrl

--- app/src/main/java/com/example/appstoreunderstan/StoreEntity.kt
+++ app/src/main/java/com/example/appstoreunderstan/StoreEntity.kt
@@ -5,8 +5,10 @@ import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "StoreEntity")//Convertimos  la data class en una entidad
-data class StoreEntity(@PrimaryKey(autoGenerate = true) var id:Long=0,
+data class StoreEntity(
+                       @PrimaryKey(autoGenerate = true) var id:Long=0,
                        var name:String,
-                       var phone:String="",
+                       var phone:String,
                        var website:String="",
+                       var photoUrl:String,
                        var isFavorite:Boolean=false)

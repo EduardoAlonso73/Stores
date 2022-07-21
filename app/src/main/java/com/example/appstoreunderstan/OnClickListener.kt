ecd686baf03e9029fa6a9057a17421d0eac73355
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-07-21 10:35:55 -0500

    Cambiamos los parametros de metodo onClick de la interfaz OnClickListener

--- app/src/main/java/com/example/appstoreunderstan/OnClickListener.kt
+++ app/src/main/java/com/example/appstoreunderstan/OnClickListener.kt
@@ -3,7 +3,7 @@ package com.example.appstoreunderstan
 import com.example.stores.StoreEntity
 
 interface OnClickListener {
-    fun onClick(storeEntity: StoreEntity){}
+    fun onClick(storeId: Long){}
     fun onFavoriteStore(storeEntity: StoreEntity){}
     fun onDeleteStore(storeEntity: StoreEntity){}
 

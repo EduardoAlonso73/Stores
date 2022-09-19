8b2bc3e073785fad5322e9825b3aafba803bc69c
Eduardo <eduardo.alonso.73h@gmail.com>
2022-09-19 14:12:18 -0500

    Configuramos  el EditStoreViewModel para ser observable

--- app/src/main/java/com/example/appstoreunderstan/mainModule/MainActivity.kt
+++ app/src/main/java/com/example/appstoreunderstan/mainModule/MainActivity.kt
@@ -4,15 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.appstoreunderstan.*
-import com.example.appstoreunderstan.common.utils.MainAux
 import com.example.appstoreunderstan.common.entities.StoreEntity
 import com.example.appstoreunderstan.databinding.ActivityMainBinding
 import com.example.appstoreunderstan.editModel.EditStoreFragment
+import com.example.appstoreunderstan.editModel.viewModel.EditStoreViewModel
 import com.example.appstoreunderstan.mainModule.adapter.OnClickListener
 import com.example.appstoreunderstan.mainModule.adapter.StoreAdapter
 import com.example.appstoreunderstan.mainModule.viewModel.MainViewModel
@@ -22,14 +21,15 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 
-class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
+class MainActivity : AppCompatActivity(), OnClickListener {
 
     private lateinit var  mBinding: ActivityMainBinding
     private lateinit var  mAdapter: StoreAdapter
     private lateinit var mGridLayout: GridLayoutManager
-    private var mainMenu: Menu? = null
     //MVVM
     private  lateinit var mMainViewModel: MainViewModel
+    private  lateinit var mEditStoreViewModel:EditStoreViewModel
+    //
 
 
 
@@ -43,16 +43,20 @@ class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
     }
 
 
-    private  fun setupViewModel(){
-        mMainViewModel=ViewModelProvider(this)[MainViewModel::class.java]
-        mMainViewModel.getStores().observe(this){store->
-            mAdapter.setListStore(store)
+    private  fun setupViewModel() {
 
+        mMainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mMainViewModel.getStores().observe(this) { store -> mAdapter.setListStore(store) }
+
+        mEditStoreViewModel = ViewModelProvider(this)[EditStoreViewModel::class.java]
+        mEditStoreViewModel.getShowFab().observe(this) { isVisible ->
+            if (isVisible) mBinding.btnFa.show() else mBinding.btnFa.hide()
         }
+
     }
 
-
     private fun launchEditFragment(args:Bundle?=null){
+            mEditStoreViewModel.setShowFab(false)
             val fragment= EditStoreFragment()
             if(args!=null){fragment.arguments=args}
             val fragmentManager=supportFragmentManager
@@ -60,14 +64,13 @@ class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
             fragmentTransaction.add(R.id.contreinerMain,fragment)
             fragmentTransaction.addToBackStack(null)// Me permite regresar al main activity
             fragmentTransaction.commit()
-            hideBtnFb(false)
+            //hideBtnFb(false)
 
         }
 
     private  fun initRecycleView(){
         mAdapter= StoreAdapter(mutableListOf(),this)
         mGridLayout=GridLayoutManager(this,2)
-        //getListStore()
         mBinding.recyclerView.apply {
             setHasFixedSize(true)
             layoutManager=mGridLayout
@@ -148,25 +151,5 @@ class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
     }
 
 
-    /* ==============================================
-                 interface  MainAux
-     ============================================== */
-    override fun hideBtnFb(isVisible: Boolean) {
-        if(isVisible) mBinding.btnFa.show() else mBinding.btnFa.hide()
-    }
+   }
 
-    override fun hideMenu(menuVisibility: Boolean) {
-        mainMenu?.findItem(R.id.action_row)?.isVisible = menuVisibility
-        mainMenu?.findItem(R.id.action_grid2)?.isVisible = menuVisibility
-        Toast.makeText(this,"Hola mundo",Toast.LENGTH_SHORT).show()
-    }
-
-    override fun addStore(storeEntity: StoreEntity) {
-        mAdapter.addStore(storeEntity)
-    }
-
-    override fun updateStore(storeEntity: StoreEntity) {
-   // mAdapter.updateStore(storeEntity)
-    }
-
-}

890a3ba242b66a2f5b7b0e9f37d5913f88fc4b53
Eduardo <eduardo.alonso.73h@gmail.com>
2022-09-19 14:04:04 -0500

    Creamos las variables observables  y sus getting y setting ademas creamos las fuciones para interactual EditStoreInteraction

--- app/src/main/java/com/example/appstoreunderstan/editModel/viewModel/EditStoreViewModel.kt
+++ app/src/main/java/com/example/appstoreunderstan/editModel/viewModel/EditStoreViewModel.kt
@@ -1,4 +1,52 @@
 package com.example.appstoreunderstan.editModel.viewModel
 
-class EditStoreViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.appstoreunderstan.common.entities.StoreEntity
+import com.example.appstoreunderstan.editModel.model.EditStoreInteractor
+
+class EditStoreViewModel:ViewModel() {
+    private  val storeSelected=MutableLiveData<StoreEntity>()
+    private  val showFab=MutableLiveData<Boolean>()
+    private val result=MutableLiveData<Any>()
+
+
+    private val  interactor:EditStoreInteractor = EditStoreInteractor()
+
+    // -=-=-=-= Get and set  storeSelected =-=-=-=-=
+    fun setStoreSelectored(storeEntity: StoreEntity){
+        this.storeSelected.value=storeEntity
+    }
+    fun getStoreSelect():LiveData<StoreEntity> = storeSelected
+
+
+    // -=-=-=-= Get and set  showFab =-=-=-=-=
+    fun setShowFab(isVisible:Boolean){ this.showFab.value=isVisible }
+    fun getShowFab():LiveData<Boolean> = showFab
+
+
+    // -=-=-=-= Get and set  result =-=-=-=-=
+    fun setResult(value:Any){ this.result.value=value }
+    fun getResult ():LiveData<Any> =  result
+
+
+
+
+    // ====== Function that involves to EditStoreInteraction ======
+
+    fun saveStore(storeEntity: StoreEntity){
+        interactor.saveStore(storeEntity){newId->
+            this.result.value=newId
+        }
+    }
+
+
+    fun updateStore(storeEntity: StoreEntity){
+        interactor.updateStore(storeEntity){storeUpdated->
+            result.value=storeUpdated
+        }
+    }
+
+
 }

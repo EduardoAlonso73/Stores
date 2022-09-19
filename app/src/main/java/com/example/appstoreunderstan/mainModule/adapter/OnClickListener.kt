7ef637298e2021cc34a5911a5ec516da8b67c93e
Eduardo <eduardo.alonso.73h@gmail.com>
2022-09-19 15:42:46 -0500

    Cambiamos el tipo de parametro de la fucion onClick de long a StoreEntity

--- app/src/main/java/com/example/appstoreunderstan/mainModule/adapter/OnClickListener.kt
+++ app/src/main/java/com/example/appstoreunderstan/mainModule/adapter/OnClickListener.kt
@@ -3,7 +3,7 @@ package com.example.appstoreunderstan.mainModule.adapter
 import com.example.appstoreunderstan.common.entities.StoreEntity
 
 interface OnClickListener {
-    fun onClick(storeId: Long){}
+    fun onClick(storeEntity: StoreEntity){}
     fun onFavoriteStore(storeEntity: StoreEntity){}
     fun onDeleteStore(storeEntity: StoreEntity){}
 

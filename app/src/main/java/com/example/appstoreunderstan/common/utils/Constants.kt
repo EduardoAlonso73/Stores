5495d1a6ac8a190a716ee959aa45c932675e4248
Eduardo <eduardo.alonso.73h@gmail.com>
2022-09-25 12:56:32 -0500

    Creamos un object  para las constantes usadas en MainInteraction

--- app/src/main/java/com/example/appstoreunderstan/common/utils/Constants.kt
+++ app/src/main/java/com/example/appstoreunderstan/common/utils/Constants.kt
@@ -1,4 +1,12 @@
 package com.example.appstoreunderstan.common.utils
 
-enum class Constants {
+object Constants {
+    const val STORES_URL ="https://store.free.beeceptor.com"
+    const val  GET_ALL_PATH="/my/api/path"
+
+    const val STATUS_PROPERTY="status"
+    const val  STORES_PROPERTY="stores"
+
+    const val SUCCESS=1
+    const val  ERROR=2
 }

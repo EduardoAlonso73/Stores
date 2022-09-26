1f1bbe1f7a60905912742fc90ce79b96dee1b846
Eduardo <eduardo.alonso.73h@gmail.com>
2022-09-26 09:47:12 -0500

    Validamos el status de la consulta

--- app/src/main/java/com/example/appstoreunderstan/mainModule/model/MainInteraction.kt
+++ app/src/main/java/com/example/appstoreunderstan/mainModule/model/MainInteraction.kt
@@ -9,16 +9,30 @@ import com.example.appstoreunderstan.StoreApplication
 import com.example.appstoreunderstan.common.entities.StoreEntity
 import com.example.appstoreunderstan.common.utils.Constants
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class MainInteraction {
 
-
     fun getStore(callback: (MutableList<StoreEntity>) -> Unit){
         val url=Constants.STORES_URL+Constants.GET_ALL_PATH
         val jsonObjectRequest=JsonObjectRequest(Request.Method.GET,url,null,{response->
+          //val status =response.getInt(Constants.STATUS_PROPERTY)
+            val status =response.optInt(Constants.STATUS_PROPERTY,Constants.ERROR)
             Log.i("Response",response.toString())
+
+            if(status == Constants.SUCCESS){
+                val jsonList =response.getJSONArray(Constants.STORES_PROPERTY).toString()
+                println("1- jsonList ==== $jsonList")
+                // TypeToken is used to tell Gson what exactly you want your string to get converted to
+                val mutableListType=object:TypeToken<MutableList<StoreEntity>>(){}.type
+                println("2- mutableListType ==== ${mutableListType}")
+                val storeList=Gson().fromJson<MutableList<StoreEntity>>(jsonList,mutableListType)
+                callback(storeList)
+                println("3- StoreList ==== ${storeList}")
+               // Log.i("Status ",status.toString())
+            }
 
         },{
             it.printStackTrace()

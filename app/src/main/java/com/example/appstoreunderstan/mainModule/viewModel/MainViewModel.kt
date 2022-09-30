566a03c6bb301f6fdd9aa19c87d382bedc13f4f1
Eduardo <eduardo.alonso.73h@gmail.com>
2022-09-30 09:34:37 -0500

    Creamos la variable observable showProgres bar

--- app/src/main/java/com/example/appstoreunderstan/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/appstoreunderstan/mainModule/viewModel/MainViewModel.kt
@@ -4,21 +4,26 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.appstoreunderstan.common.entities.StoreEntity
+import com.example.appstoreunderstan.common.utils.Constants
 import com.example.appstoreunderstan.mainModule.model.MainInteraction
 
 
 class MainViewModel:ViewModel() {
     private    var storeList:MutableList<StoreEntity> = mutableListOf()
     private  var interactor:MainInteraction= MainInteraction()
+    private  val showProgress:MutableLiveData<Boolean> = MutableLiveData()
 
     // lazy is other way to initialization  variable of type --val--
     private val  store:MutableLiveData<MutableList<StoreEntity>>  by lazy { MutableLiveData<MutableList<StoreEntity>>()
         .also {  loadStore() } }
-
-
     fun getStores():LiveData<MutableList<StoreEntity>> =store
+
+
+    fun isShowProgressBar():LiveData<Boolean> =showProgress
     private fun loadStore(){
+        showProgress.value=Constants.SHOW //Inicia apiori mostrando el progress bar
         interactor.getStore {
+            showProgress.value=Constants.HIDE
             store.value=it
             storeList=it
         }

1b305f14c5e9c677854fd159edbb94336a087ee2
Eduardo <eduardo.alonso.73h@gmail.com>
2022-10-02 10:28:18 -0500

    Creamos la fucion getStoreById para la consulta puntual

--- app/src/main/java/com/example/appstoreunderstan/editModel/model/EditStoreInteractor.kt
+++ app/src/main/java/com/example/appstoreunderstan/editModel/model/EditStoreInteractor.kt
@@ -1,11 +1,16 @@
 package com.example.appstoreunderstan.editModel.model
 
+import androidx.lifecycle.LiveData
 import com.example.appstoreunderstan.StoreApplication
 import com.example.appstoreunderstan.common.entities.StoreEntity
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class EditStoreInteractor {
+
+    fun getStoreById(id:Long): LiveData<StoreEntity> {
+        return  StoreApplication.database.storeDoa().getStoreById(id)
+    }
 
     fun saveStore(storeEntity: StoreEntity, callback: (Long) -> Unit){
         doAsync {

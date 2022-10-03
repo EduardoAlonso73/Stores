7564eb17582ddc8d55aebcf29a7a8294e7583beb
Eduardo <eduardo.alonso.73h@gmail.com>
2022-10-03 14:44:26 -0500

    Agregamos la fucion executeAction para ejecutar acciones de EditInteraction

--- app/src/main/java/com/example/appstoreunderstan/editModel/viewModel/EditStoreViewModel.kt
+++ app/src/main/java/com/example/appstoreunderstan/editModel/viewModel/EditStoreViewModel.kt
@@ -3,14 +3,21 @@ package com.example.appstoreunderstan.editModel.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.appstoreunderstan.common.entities.StoreEntity
+import com.example.appstoreunderstan.common.utils.Constants
+import com.example.appstoreunderstan.common.utils.StoresExceptio
+import com.example.appstoreunderstan.common.utils.TypeError
 import com.example.appstoreunderstan.editModel.model.EditStoreInteractor
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class EditStoreViewModel:ViewModel() {
     private  var storeId:Long=0
     private  val storeSelected=MutableLiveData<StoreEntity>()
     private  val showFab=MutableLiveData<Boolean>()
     private val result=MutableLiveData<Any>()
+    private  val typeError:MutableLiveData<TypeError> = MutableLiveData()
 
 
     private val  interactor:EditStoreInteractor = EditStoreInteractor()
@@ -37,6 +44,7 @@ class EditStoreViewModel:ViewModel() {
     // -=-=-=-= Get and set  result =-=-=-=-=
     fun setResult(value:Any){ this.result.value=value }
     fun getResult ():LiveData<Any> =  result //🚀 👀
+    fun getTypeError():MutableLiveData<TypeError> = typeError
 
 
 
@@ -44,17 +52,24 @@ class EditStoreViewModel:ViewModel() {
     // ====== Function that involves to EditStoreInteraction ======
 
     fun saveStore(storeEntity: StoreEntity){
-        interactor.saveStore(storeEntity){newId->
-            setResult(newId)
-        }
+     executeAction(storeEntity) { interactor.saveStore(storeEntity) }
     }
-
 
     fun updateStore(storeEntity: StoreEntity){
-        interactor.updateStore(storeEntity){storeUpdated->
-            setResult(storeUpdated)
-        }
+     executeAction(storeEntity) {interactor.updateStore(storeEntity) }
     }
 
+
+    private fun executeAction(storeEntity: StoreEntity,block:suspend () -> Unit): Job {
+        return viewModelScope.launch {
+            try {
+                block()
+                result.value=storeEntity
+            }catch (e: StoresExceptio){
+                typeError.value=e.typeError
+            }
+
+        }
+    }
 
 }

a6a6275299e668c002b1eb57b8fa92ab4c349f59
Eduardo <eduardo.alonso.73h@gmail.com>
2022-10-04 09:28:34 -0500

    Definimos la sentencia SQL para definir el indice asi mismo de la version de la modificacio

--- app/src/main/java/com/example/appstoreunderstan/common/database/StoreDatabase.kt
+++ app/src/main/java/com/example/appstoreunderstan/common/database/StoreDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.appstoreunderstan.common.database.StoreDao
 import com.example.appstoreunderstan.common.entities.StoreEntity
 
-@Database(entities= arrayOf(StoreEntity::class), version = 2 )
+@Database(entities= arrayOf(StoreEntity::class), version = 3 )
  abstract  class StoreDatabase: RoomDatabase() {
 
      abstract  fun storeDoa(): StoreDao
